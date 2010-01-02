28c0530bbfb1383ef8aacc924a22ed0f3f387ab0
jwalters <jwalters@814030fe-4239-4656-a315-6a5994094d37>
2010-01-02 13:54:47 +0000

    Initial import
    git-svn-id: https://svn.code.sf.net/p/remoteteanet/code/trunk@2 814030fe-4239-4656-a315-6a5994094d37

---
+++ jrpcgen/src/org/acplt/oncrpc/apps/jrpcgen/JrpcgenParamInfo.cs
@@ -0,0 +1,35 @@
+namespace org.acplt.oncrpc.apps.jrpcgen
+{
+	/// <summary>
+	/// The <code>JrpcgenParamInfo</code> class contains information about the
+	/// data type of a procedure's parameter, as well as the parameter's optional
+	/// name.
+	/// </summary>
+	/// <remarks>
+	/// The <code>JrpcgenParamInfo</code> class contains information about the
+	/// data type of a procedure's parameter, as well as the parameter's optional
+	/// name.
+	/// </remarks>
+	/// <version>$Revision: 1.2 $ $Date: 2003/08/14 08:09:59 $ $State: Exp $ $Locker:  $</version>
+	/// <author>Harald Albrecht</author>
+	internal class JrpcgenParamInfo
+	{
+		public string parameterType;
+
+		public string parameterName;
+
+		/// <summary>
+		/// Constructs a new <code>JrpcgenParamInfo</code> object containing
+		/// information about ...
+		/// </summary>
+		/// <remarks>
+		/// Constructs a new <code>JrpcgenParamInfo</code> object containing
+		/// information about ...
+		/// </remarks>
+		public JrpcgenParamInfo(string parameterType, string parameterName)
+		{
+			this.parameterType = parameterType;
+			this.parameterName = parameterName;
+		}
+	}
+}
